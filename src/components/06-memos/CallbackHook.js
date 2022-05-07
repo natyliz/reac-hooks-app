4522d8f9eba28b3161aef6a2fa8a29b9ce66250a
Nathaly Colcha <natyliz_1792@hotmail.com>
2022-05-07 18:41:07 -0500

    ultimos cambios del dia 07052022

--- src/components/06-memos/CallbackHook.js
+++ src/components/06-memos/CallbackHook.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import '../02-useEffect/effects.css';
+import { ShowIncrement } from './ShowIncrement';
 
 export const CallbackHook = () => {
+  const [counter, setCounter] = useState(10);
+  // const increment = () => {
+  //   setCounter(counter +1);
+  // }
+
+const increment =useCallback((num) => {
+  setCounter( (c) => c +num);
+    
+  },[setCounter]);
+
   return (
     <div>
-        <h1>UseCallback HookApp</h1>
+        <h1>UseCallback Hook: {counter}</h1>
         <hr/>
+        <ShowIncrement increment={increment}/>
+
+
         </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { CallbackHook } from './components/06-memos/CallbackHook';
+//import { CallbackHook } from './components/06-memos/CallbackHook';
+import { Padre } from './components/07-tarea-memo/Padre';
 //import { MemoHook } from './components/06-memos/MemoHook';
 //import { RealExampleRef } from './components/04-useRef/RealExampleRef';
 //import { Layout } from './components/05-useLayoutEffect/Layout';
@@ -16,6 +17,6 @@ import { CallbackHook } from './components/06-memos/CallbackHook';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <CallbackHook />
+    <Padre />
  );
 
